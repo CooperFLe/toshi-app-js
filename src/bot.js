7c5e7ab6984445b66eff0d3708e3393f549a1f62
Cooper F. Le <cooperfle@utdallas.edu>
2017-08-04 23:00:25 -0500

    Add commands and responses for Coinbase account setup

--- src/bot.js
+++ src/bot.js
@@ -35,7 +35,19 @@ bot.onEvent = function(session, message) {
 
 function onMessage(session, message) {
   if(message.body.toUpperCase() == 'HELP'){
-    sendMessage(session,'This is a work in progress. Nothing works right now.')
+    let whatis = "Turtled Bot is a simple bot I created to view my Coinbase balances. In order to use this bot, you will need to submit your API key because I am too lazy to learn how to implement to Oath2 token. I am not storing this key in anyway, but you don't have to believe me. The source code can be found at https://github.com/cooperfle/toshi-app-js"
+    let controls = [
+      {type: 'button', label: 'Set up Coinbase', value: 'api'},
+      {type: 'button', label: 'Reset', value: 'reset'},
+      {type: 'button', label: 'Donate', value: 'donate'}
+    ]
+    
+    session.reply(SOFA.Message({
+    body: whatis,
+    controls: controls,
+    showKeyboard: false,
+  }))
+    
   }else
     if(message.body == 'Give me a turtle'){
       session.reply(SOFA.Message({
@@ -54,6 +66,9 @@ function onCommand(session, command) {
   switch (command.content.value) {
     case 'ping':
       pong(session)
+      break
+    case 'api':
+      api(session)
       break
     case 'count':
       count(session)
@@ -93,6 +108,17 @@ function onPayment(session, message) {
 
 function welcome(session) {
   sendMessage(session, `Hello Token!`)
+}
+
+function api(session) {
+  session.reply(SOFA.Message({
+    body: 'You need to get your API key from Coinbase at https://www.coinbase.com/settings/api',
+    showKeyboard: false,
+  }))
+  session.reply(SOFA.Message({
+    body: 'I will need access to all the wallets you want to see the balances of and the ability to read your accounts.',
+    showKeyboard: true,
+  }))
 }
 
 function pong(session) {
